bb6b4080ecd22872dc45195a96f8dc960f2cff99
Abhishek Chaurasiya <abhichek2580@gmail.com>
2023-04-17 11:01:23 +0530

    add page

--- src/pages/404/PageNotFound.jsx
+++ src/pages/404/PageNotFound.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
-import "./PageNotFound.scss"
+import React from "react";
+
+import "./PageNotFound.scss";
+
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
 
 const PageNotFound = () => {
   return (
-    <div>
-      I'm PageNotFound page
+    <div className="pageNotFound">
+      <ContentWrapper>
+        <span className="bigText">404</span>
+        <span className="smallText">Page not found!</span>
+      </ContentWrapper>
     </div>
-  )
-}
+  );
+};
 
-export default PageNotFound
+export default PageNotFound;
